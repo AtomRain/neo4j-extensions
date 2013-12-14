df5d430bebcd12095a9b6c6c67d6dc5f6af45f75
Brad Nussbaum <brad@atomrain.com>
2013-12-13 16:23:01 -0800

    Fixing indexing. Removing username.

--- neo4j-extensions-java/src/main/java/org/neo4j/extensions/java/rest/UserController.java
+++ neo4j-extensions-java/src/main/java/org/neo4j/extensions/java/rest/UserController.java
@@ -4,17 +4,18 @@ import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.neo4j.extensions.java.domain.User;
 import org.neo4j.extensions.java.indexes.UidsIndex;
-import org.neo4j.extensions.java.indexes.UserIndex;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
@@ -39,20 +40,21 @@ public class UserController {
     private GraphDatabaseService db;
 
     /**
-     * @return Status 200 on success.
+     * @return Status 201 on success.
      */
-    @GET
-    @Path("/json")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response create(@QueryParam("username") String username, @QueryParam("indexingOn") Boolean indexingOn) {
-        LOGGER.info(String.format("/user/create/json?username=%s", username));
+    @POST
+    @Path("/create")
+    @Produces({
+        MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML
+    })
+    public Response create(@QueryParam("indexingOn") @DefaultValue("true") Boolean indexingOn) {
+        LOGGER.info(String.format("POST /user/create?indexingOn=%s", indexingOn));
         long startTimeTx = System.currentTimeMillis();
 
         Index<Node> uidsIndex = db.index().forNodes(UidsIndex.uid.getIndexName(), UidsIndex.uid.getIndexType());
 
         Transaction tx = null;
         User user = new User();
-        user.setUsername(username);
         try {
             tx = db.beginTx();
 
@@ -75,31 +77,28 @@ public class UserController {
                 uidsIndex.putIfAbsent(userNode, UidsIndex.uid.name(), userUid);
             }
 
-            // establish username
-            userNode.setProperty("username", username);
-            user.setUsername(username);
-            if (indexingOn) {
-                uidsIndex.putIfAbsent(userNode, UserIndex.username.name(), username);
-            }
-
             // capture times
             long successTimeTx = System.currentTimeMillis();
             long processTimeTx = successTimeTx - startTimeTx;
 
+            tx.success();
+
             // log details
-            LOGGER.log(Level.FINE,
+            LOGGER.log(Level.INFO,
                     String.format("UserController: TX: userId=%s, userUid=%s, processTime=%dms", userId, userUid, processTimeTx));
 
-            tx.success();
+            return Response.status(Status.CREATED).entity(user).build();
         } catch (Exception e) {
+            user = null;
             tx.failure();
+            // log details
+            LOGGER.log(Level.SEVERE, e.getMessage());
         } finally {
             if (tx != null) {
                 tx.finish();
             }
         }
-
-        return Response.ok().entity(user).build();
+        return Response.serverError().build();
     }
 
 }
