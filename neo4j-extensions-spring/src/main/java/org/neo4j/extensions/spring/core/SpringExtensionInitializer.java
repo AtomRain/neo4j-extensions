ae8ec8e050673d0ff837c0a06d7bdba746610241
Brad Nussbaum <brad@atomrain.com>
2014-02-03 10:18:31 -0800

    Adding spring extension testing.

--- neo4j-extensions-spring/src/main/java/org/neo4j/extensions/spring/core/SpringExtensionInitializer.java
+++ neo4j-extensions-spring/src/main/java/org/neo4j/extensions/spring/core/SpringExtensionInitializer.java
@@ -2,23 +2,20 @@ package org.neo4j.extensions.spring.core;
 
 import org.neo4j.extensions.spring.repository.UserRepository;
 import org.springframework.data.neo4j.server.SpringPluginInitializer;
-import org.springframework.data.neo4j.support.Neo4jTemplate;
 
 /**
  * Hook for Spring initialization
- * 
+ *
  * @author bradnussbaum
  * @version 0.1.0
- * 
  * @since 0.1.0
  */
 public class SpringExtensionInitializer extends SpringPluginInitializer {
 
-    @SuppressWarnings("unchecked")
     public SpringExtensionInitializer() {
-        super(new String[] {
+        super(new String[]{
                 "META-INF/spring/springContext.xml"
-        }, expose("template", Neo4jTemplate.class), expose("userRepository", UserRepository.class));
+        }, expose("userRepositoryImpl", UserRepository.class));
     }
 
 }
