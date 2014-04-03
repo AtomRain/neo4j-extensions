165877124cb63b617e41116dd8e6054d79730b5e
joseph.moore <joseph.moore@atomrain.com>
2014-04-02 22:33:34 -0700

    fix context file

--- neo4j-extensions-spring/src/test/java/org/neo4j/extensions/spring/rest/DeployTest.java
+++ neo4j-extensions-spring/src/test/java/org/neo4j/extensions/spring/rest/DeployTest.java
@@ -1,16 +1,17 @@
 package org.neo4j.extensions.spring.rest;
 
-import static junit.framework.Assert.assertEquals;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 
+import javax.ws.rs.core.MediaType;
+
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.neo4j.graphdb.Transaction;
 import org.neo4j.kernel.GraphDatabaseAPI;
 import org.neo4j.server.CommunityNeoServer;
 import org.neo4j.server.helpers.CommunityServerBuilder;
@@ -44,18 +45,19 @@ public class DeployTest {
 
     @Test
     public void shouldReturnAllTheNodes() {
-        Transaction tx = db.beginTx();
-        db.createNode().setProperty("name", "Mark");
-        db.createNode().setProperty("name", "Dave");
-        tx.success();
-        tx.close();
+//        Transaction tx = db.beginTx();
+//        db.createNode().setProperty("name", "Mark");
+//        db.createNode().setProperty("name", "Dave");
+//        tx.success();
+//        tx.close();
 
-//        JsonNode response = jerseyClient()
-//                .resource(server.baseUri().toString() + "unmanaged/dummy/all-nodes")
-//                .get(ClientResponse.class)
-//                .getEntity(JsonNode.class);
-//
-//        assertEquals("Dave", response.get("n.name").get(0).asText());
+        JsonNode response = jerseyClient()
+                .resource(server.baseUri().toString() + "extensions-java/user/create?indexingOn=false")
+                .accept(MediaType.APPLICATION_JSON)
+                .post(ClientResponse.class)
+                .getEntity(JsonNode.class);
+
+        Assert.assertNotNull(response.get("n.user").get(0).asText());
 //        assertEquals("Mark", response.get("n.name").get(1).asText());
     }
 
