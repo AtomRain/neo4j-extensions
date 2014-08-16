a1454cb83c177e67c751a4cf90e9cd8f7631f39f
Brad Nussbaum <brad@atomrain.com>
2014-08-16 14:52:31 -0700

    Enhancing properties loading

--- neo4j-extensions-spring/src/test/java/org/neo4j/extensions/spring/rest/UserControllerTest.java
+++ neo4j-extensions-spring/src/test/java/org/neo4j/extensions/spring/rest/UserControllerTest.java
@@ -1,4 +1,4 @@
-package org.neo4j.extensions.java.rest;
+package org.neo4j.extensions.spring.rest;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
@@ -8,81 +8,59 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.neo4j.extensions.common.domain.FriendResult;
+import org.junit.runner.RunWith;
+import org.neo4j.extensions.spring.domain.FriendResult;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.server.CommunityNeoServer;
 import org.neo4j.server.helpers.CommunityServerBuilder;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 import java.util.logging.Logger;
 
 /**
  * @author bradnussbaum
  * @since 2014-05-25
  */
+@Configurable
+@ContextConfiguration("classpath*:META-INF/spring/test-springContext.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
 public class UserControllerTest {
 
     private static final Logger LOGGER = Logger.getLogger(UserControllerTest.class.getName());
 
-    Properties properties = new Properties();
-    InputStream input = null;
+    @Value("${neo4j.server.port}")
+    private Integer neo4jServerPort;
 
-    private String neo4jServerPort = System.getProperty("neo4j.server.port");
+    @Value("${neo4j.remoteShell.port}")
+    private Integer neo4jRemoteShellPort;
 
-    private String neo4jRemoteShellPort = System.getProperty("neo4j.remoteShell.port");
-
-    private String neo4jGraphDb = System.getProperty("neo4j.graph.db");
+    @Value("${neo4j.graph.db}")
+    private String neo4jGraphDb;
 
     private GraphDatabaseService db;
     private CommunityNeoServer server;
 
     @Before
     public void before() throws IOException {
-        try {
-            input = UserControllerTest.class.getClassLoader().getResourceAsStream("neo4j-extensions-java-maven.properties");
-            // load the properties file
-            properties.load(input);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (input != null) {
-                try {
-                    input.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        // allow system properties to override
-        if (neo4jServerPort == null) {
-            neo4jServerPort = properties.getProperty("neo4j.server.port");
-        }
-        if (neo4jRemoteShellPort == null) {
-            neo4jRemoteShellPort = properties.getProperty("neo4j.remoteShell.port");
-        }
-        if (neo4jGraphDb == null) {
-            neo4jGraphDb = properties.getProperty("neo4j.graph.db");
-        }
-
-        LOGGER.info(String.format("neo4jServerPort: %s)", neo4jServerPort));
-        LOGGER.info(String.format("neo4jRemoteShellPort: %s)", neo4jRemoteShellPort));
+        LOGGER.info(String.format("neo4jServerPort: %d)", neo4jServerPort));
+        LOGGER.info(String.format("neo4jRemoteShellPort: %d)", neo4jRemoteShellPort));
         LOGGER.info(String.format("neo4jGraphDb: %s)", neo4jGraphDb));
 
-        // build the server
         server = CommunityServerBuilder
                 .server()
                 .usingDatabaseDir(neo4jGraphDb)
-                .onPort(Integer.valueOf(neo4jServerPort))
+                .onPort(neo4jServerPort)
                 .withDefaultDatabaseTuning()
-                .withThirdPartyJaxRsPackage("org.neo4j.extensions.java", "/extensions-java")
+                .withThirdPartyJaxRsPackage("org.neo4j.extensions.spring", "/extensions-spring")
                 .withProperty("remote_shell_enabled", "false")
-                .withProperty("remote_shell_port", neo4jRemoteShellPort)
+                .withProperty("remote_shell_port", neo4jRemoteShellPort.toString())
                 .build();
         server.start();
         db = server.getDatabase().getGraph();
@@ -97,7 +75,7 @@ public class UserControllerTest {
     public void testCreateUser() {
 
         ClientResponse response = jerseyClient()
-                .resource(server.baseUri().toString() + "extensions-java/user/create?indexingOn=true")
+                .resource(server.baseUri().toString() + "extensions-spring/user/create?indexingOn=true")
                 .accept(MediaType.APPLICATION_JSON)
                 .post(ClientResponse.class);
         FriendResult result = response.getEntity(FriendResult.class);
